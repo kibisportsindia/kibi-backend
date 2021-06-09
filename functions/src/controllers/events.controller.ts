c4793d7348bc4b09a447ae95121a27e188c34882
Manjot Singh <manjot.singh@tatadigital.com>
2021-06-09 06:54:02 +0530

    commented key file name

--- functions/src/controllers/events.controller.ts
+++ functions/src/controllers/events.controller.ts
@@ -7,13 +7,13 @@ import { Storage } from "@google-cloud/storage";
 const formParser = require("../utils/formParser");
 const MAX_SIZE = 4000000; // 4MB
 
-admin.initializeApp(functions.config().firebase,"app");
+admin.initializeApp(functions.config().firebase, "app");
 export let db = admin.firestore();
 const eventCollection = "events";
 
 const storage = new Storage({
-  projectId: config.project_id,
-  keyFilename: "../config/config.json"
+  projectId: config.project_id
+  // keyFilename: "./config/config.json"
 });
 
 const bucket = storage.bucket(`${config.project_id}.appspot.com`);
@@ -27,7 +27,7 @@ export const addEvent = async (
   const file = formData.files[0];
   console.log("formdata ", formData);
   console.log("file ", file);
-  console.log("Buffer",file.content);
+  console.log("Buffer", file.content);
   try {
     if (!file) {
       res.status(400).send("Error, could not upload file ( file not found )");
@@ -41,7 +41,9 @@ export const addEvent = async (
     });
     blobWriter.on("error", err => next(err));
     blobWriter.on("finish", async () => {
-      const publicUrl = `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/${encodeURI(blob.name)}?alt=media`;
+      const publicUrl = `https://firebasestorage.googleapis.com/v0/b/${
+        bucket.name
+      }/o/${encodeURI(blob.name)}?alt=media`;
 
       console.log("url", publicUrl);
       const event: Event = {
@@ -59,7 +61,7 @@ export const addEvent = async (
       };
       console.log("event is ", event);
       const newDoc = await db.collection(eventCollection).add(event);
-      res.status(200).send({ message: "Event added", id:newDoc.id });
+      res.status(200).send({ message: "Event added", id: newDoc.id });
     });
     blobWriter.end(file.content);
   } catch (error) {
@@ -88,11 +90,11 @@ export const getEvents = async (
 
         let data = [];
         eventData.forEach(doc => {
-          let id=doc.id;
-          let docData = {id,...doc.data()}
-          data.push(docData)
-        }); 
-        console.log(data)
+          let id = doc.id;
+          let docData = { id, ...doc.data() };
+          data.push(docData);
+        });
+        console.log(data);
         res.status(200).json(data);
       });
   } catch (err) {
@@ -101,10 +103,6 @@ export const getEvents = async (
     return;
   }
 };
-
-
-
-
 
 export const updateEvent = async (
   req: Request,
@@ -115,61 +113,67 @@ export const updateEvent = async (
   const file = formData.files[0];
   try {
     if (!file) {
-      res.status(400).json({message:"Error, file not found!"});
+      res.status(400).json({ message: "Error, file not found!" });
       return;
     }
     let id = formData["id"];
-    console.log(id)
-    db.collection(eventCollection).doc(id).get().then(async doc=>{
-      let imageUrl = doc.data().image
-      const fileName = imageUrl.split("o/")[1].split("?")[0];
-      console.log("fileName",fileName);
-      const img = bucket.file(fileName)
-      img.delete().then(result=>{
-        const blob = bucket.file(file.filename);
-        const blobWriter = blob.createWriteStream({
-          metadata: {
-            contentType: file.contentType
-          }
+    console.log(id);
+    db.collection(eventCollection)
+      .doc(id)
+      .get()
+      .then(async doc => {
+        let imageUrl = doc.data().image;
+        const fileName = imageUrl.split("o/")[1].split("?")[0];
+        console.log("fileName", fileName);
+        const img = bucket.file(fileName);
+        img.delete().then(result => {
+          const blob = bucket.file(file.filename);
+          const blobWriter = blob.createWriteStream({
+            metadata: {
+              contentType: file.contentType
+            }
+          });
+          blobWriter.on("error", err => next(err));
+          blobWriter.on("finish", async () => {
+            const publicUrl = `https://firebasestorage.googleapis.com/v0/b/${
+              bucket.name
+            }/o/${encodeURI(blob.name)}?alt=media`;
+            console.log(id);
+            db.collection(eventCollection)
+              .doc(id)
+              .update({
+                event_name: formData["event_name"],
+                date: formData["date"],
+                place: formData["place"],
+                sports: formData["sports"],
+                age_category: formData["age_category"],
+                image: publicUrl,
+                how_to_participate: formData["how_to_participate"],
+                charges: formData["charges"],
+                benefits: formData["benefits"],
+                phone: formData["phone"],
+                type: formData["type"]
+              })
+              .then(() => {
+                res.status(200).json({ messgae: "Event Update Successfully" });
+              })
+              .catch(err => {
+                console.log(1, err);
+                res.status(400).json({ messgae: "Something Went Wrong!" });
+              });
+          });
+          blobWriter.end(file.content);
         });
-        blobWriter.on("error", err => next(err));
-        blobWriter.on("finish", async () => {
-          const publicUrl = `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/${encodeURI(blob.name)}?alt=media`;
-          console.log(id)
-          db.collection(eventCollection).doc(id).update({
-            event_name: formData["event_name"],
-              date: formData["date"],
-              place: formData["place"],
-              sports: formData["sports"],
-              age_category: formData["age_category"],
-              image: publicUrl,
-              how_to_participate: formData["how_to_participate"],
-              charges: formData["charges"],
-              benefits: formData["benefits"],
-              phone: formData["phone"],
-              type: formData["type"]
-          }).then(()=>{
-            res.status(200).json({messgae:"Event Update Successfully"})
-          }).catch(err=>{
-            console.log(1,err)
-            res.status(400).json({messgae:"Something Went Wrong!"})
-          })
-        })
-        blobWriter.end(file.content);
-    })
-      }).catch(err=>{
-        console.log(1,err)
-        res.status(400).json({message:"Something went wrong!!"});
       })
+      .catch(err => {
+        console.log(1, err);
+        res.status(400).json({ message: "Something went wrong!!" });
+      });
   } catch (error) {
-    console.log(1,error)
+    console.log(1, error);
     res.status(400).send(`Something went wrong!!`);
   }
 };
-
-
-
-
 
 export const deleteEvent = async (
   req: Request,
@@ -178,21 +182,29 @@ export const deleteEvent = async (
 ) => {
   try {
     let id = req.body["event_id"];
-    db.collection(eventCollection).doc(id).get().then(async doc=>{
-      let imageUrl = doc.data().image
-      const fileName = imageUrl.split("o/")[1].split("?")[0];
-      console.log("fileName",fileName);
-      await db.collection(eventCollection).doc(id).delete();
-      const file = bucket.file(fileName)
-      file.delete().then(result=>{
-        res.status(200).json({message:"Event Deleted Successfully"});
-        console.log("FILE DELETED")
-      }).catch(err=>{
-        res.status(400).json({message:"Something went wrong!!"});
-      })
-    })
+    db.collection(eventCollection)
+      .doc(id)
+      .get()
+      .then(async doc => {
+        let imageUrl = doc.data().image;
+        const fileName = imageUrl.split("o/")[1].split("?")[0];
+        console.log("fileName", fileName);
+        await db
+          .collection(eventCollection)
+          .doc(id)
+          .delete();
+        const file = bucket.file(fileName);
+        file
+          .delete()
+          .then(result => {
+            res.status(200).json({ message: "Event Deleted Successfully" });
+            console.log("FILE DELETED");
+          })
+          .catch(err => {
+            res.status(400).json({ message: "Something went wrong!!" });
+          });
+      });
   } catch (error) {
-    res.status(400).json({message:"Something went wrong!!"});
+    res.status(400).json({ message: "Something went wrong!!" });
   }
 };
-
