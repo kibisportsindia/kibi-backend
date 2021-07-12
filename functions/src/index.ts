15dfa482b9773d0f378c0351ac2c650aa2f55442
Javoh <javoh.omonov@trustalchemy.com>
2021-07-13 00:43:30 +0500

    Initial Setup Of Socket

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,14 @@ import * as bodyParser from "body-parser";
 import * as admin from "firebase-admin";
 import * as express from "express";
 const cors = require("cors");
+import { createServer } from 'http';
+import { Server } from 'socket.io'
+import * as io from 'socket.io'
+
+// Socket configuration
+import WebSockets from "./utils/WebSockets";
+
+
 //initialize firebase inorder to access its services
 
 //initialize express server
@@ -23,5 +31,22 @@ app.use("/post", require("./routes/posts.routes"));
 app.get("/heartbeat", (req, res) => {
   res.status(200).json(`Running`);
 });
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      SocketServer: io.Server
+    }
+  }
+}
+
+/** Create HTTP server. */
+const server = createServer(app);
+
+/** Create socket connection */
+global.SocketServer = new Server(server);
+global.SocketServer.on("connection", WebSockets.connection);
+
+
 //define google cloud function name
 exports.app = functions.https.onRequest(app);
