b916e935547c480cce09796d6a8b8c71fbeb3f21
javoh-iut <javokhir.omonov@hygge.software>
2021-08-16 10:21:36 +0500

    Bug fixed

--- functions/src/routes/chatrooms.routes.ts
+++ functions/src/routes/chatrooms.routes.ts
@@ -1,14 +1,16 @@
 import * as express from "express";
 import * as ChatRoomController from "../controllers/chatroom.controllers";
-import authUser from "../middlewares/auth-user";
+const auth = require("../middlewares/auth-user");
 
 const router = express.Router();
 
 router.route("/initiate").post(ChatRoomController.initiateChatRoom);
 router
   .route("/:roomId/message")
-  .post(authUser, ChatRoomController.postMessageInChatRoom);
+  .post(auth, ChatRoomController.postMessageInChatRoom);
 router.route("/:roomId").get(ChatRoomController.getConversationByRoomId);
-router.route('/:roomId/mark-read').put(ChatRoomController.markConversationReadByRoomId);
+router
+  .route("/:roomId/mark-read")
+  .put(ChatRoomController.markConversationReadByRoomId);
 
 module.exports = router;
