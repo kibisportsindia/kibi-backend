2a68697d7aafda0c3cebbf5cad52110394ec4fd8
Jatin Gupta <56168174+jatin0814@users.noreply.github.com>
2021-08-29 19:04:37 +0530

    Update events.controller.ts

--- functions/src/controllers/events.controller.ts
+++ functions/src/controllers/events.controller.ts
@@ -173,7 +173,9 @@ export const deleteEvent = async (
         //  .catch((err) => {
         // res.status(400).json({ message: "Something went wrong!!" });
         //  });
-      });
+      }).catch(err=>{
+      res.status(200).json({ message: err.message });
+    });
   } catch (error) {
     res.status(400).json({ message: "Something went wrong!!" });
   }
